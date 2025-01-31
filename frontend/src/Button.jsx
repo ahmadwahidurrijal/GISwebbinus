0241f057fd52571d9bc867602e028e066a6a13c4
ahmadwahidurrijal <ahmad.wahidurrijal@binus.ac.id>
2025-02-01 02:43:47 +0700

    Update Button.jsx
    hapus

--- frontend/src/Button.jsx
+++ frontend/src/Button.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
 
-const Button = ({ text, onClick, type = 'button' }) => {
-    return (
-        <button className="btn-primary" onClick={onClick} type={type}>
-            {text}
-        </button>
-    );
-};
-
-export default Button;
