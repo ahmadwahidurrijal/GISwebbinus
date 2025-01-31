4c38da1118dc60b9c28b4edcd4692f18e6833efa
ahmadwahidurrijal <ahmad.wahidurrijal@binus.ac.id>
2025-02-01 03:23:11 +0700

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,21 @@
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import "leaflet/dist/leaflet.css"; // Impor CSS Leaflet
 
 function App() {
     return (
-        <>
+        <Router>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/donate" element={<Donate />} />
-                <Route path="/request" element={<Request />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
             <Footer />
-        </>
+        </Router>
     );
 }
 
